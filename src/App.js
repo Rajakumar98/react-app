262ab67a231580eede554252e968fdf67c0536ab
Rajakumar98 <rajakumar1998mech@gmail.com>
2023-03-11 12:50:44 +0530

    commit firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  //js
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Greeting/>
+
+      <Detials
+        access="✔"
+        img="https://th.bing.com/th?q=Baby+Boy+Cartoon+Characters&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247"
+        name="Rajakumar"
+        batch="B-43 guvi"
+      />
+      <Detials
+        access="✔"
+        img="https://th.bing.com/th/id/OIP.yG3zy-POBK29vC8oScSAugHaHR?w=206&h=202&c=7&r=0&o=5&pid=1.7"
+        name="Arunkumar"
+        batch="B-43 guvi"
+      />
+      <Detials
+        access="❕"
+        img="https://th.bing.com/th/id/OIP.J5dFEhklCY19hS1G3VGWeQHaOL?w=115&h=187&c=7&r=0&o=5&pid=1.7"
+        name="Thambidhuri"
+        batch="B-43 guvi"
+      />
     </div>
   );
 }
 
 export default App;
+function Detials(props) {
+  return (
+    <div className="Name">
+      <img src={props.img} alt={props.name} />
+      <h2>
+      <span>{props.access}</span>{props.name}</h2>
+      <p>{props.batch}</p>
+    </div>
+  );
+}
+function Greeting(){
+  // return(
+  //   <div>
+  //     <h1>Hi this is my first web page</h1>
+  //   </div>
+  // )
+  return React.createElement("div",{id : "div-name" , className:"div-name"},
+ React.createElement("h2",{className:"name"},"Hi  guvi. this is my first web page "))
+} 
